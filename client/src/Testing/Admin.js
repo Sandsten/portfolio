20715a3909551339da6e75c56238cf2269857b4b
Staffan Sandberg <staffan.sandberg94@gmail.com>
2019-08-14 16:56:02 +0200

    Some progress on admin page

--- client/src/Testing/Admin.js
+++ client/src/Testing/Admin.js
@@ -7,6 +7,7 @@ const URL =
 
 const Admin = () => {
   const isSignedIn = useSelector(state => state.user.signedIn);
+  const projects = useSelector(state => state.projects);
   const dispatch = useDispatch();
 
   const createAccount = event => {
@@ -51,6 +52,26 @@ const Admin = () => {
     );
   };
 
+	/*
+		STATES
+		signed in is null - we haven't checked yet
+		signed in is true - we are signed in
+		signed in is false - we are signed out
+
+		These should only be checked if we are signed in
+		projects fetched is false - projects havent been fetched yet
+		projects fetched is true - projects have been fetched
+	*/
+
+  //TODO: change to use fetched instead, so this isn't shown when we are signed out too
+  if (isSignedIn === null)
+    return <div style={{ margin: "10px" }}>Loading...</div>;
+
+	//TODO: fetch project data if it hasn't been done yet. "useEffect"
+	// If you are signed in and projects haven't been fetched yet
+  if(isSignedIn && !projects.fetched)
+    return <div style={{ margin: "10px" }}>Loading projects data...</div>;
+
   const notSignedInView = (
     <>
       <label>Create admin account</label>
@@ -73,10 +94,14 @@ const Admin = () => {
     </>
   );
 
-  const signedInView = <div>You are signed in as admin</div>;
-
-  //TODO: change to use fetched instead, so this isn't shown when we are signed out too
-  if(isSignedIn === null) return <div style={{margin: "10px"}}>Loading...</div>
+  const signedInView = (
+    <>
+      <div>You are signed in as admin</div>
+      {/* {projects.projects.map(project => {
+        return <h3>roject.title</h3>
+      })} */}
+    </>
+  );
 
   return (
     <div style={{ margin: "10px" }}>
