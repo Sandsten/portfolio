6bc441216ad299debe86cf6b388ba6cc025f04c6
Sandsten <staffansandberg@outlook.com>
2020-12-04 16:09:46 +0100

    Favicon added

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -3,67 +3,70 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 module.exports = {
-  target: 'web',
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js',
-    publicPath: '/'
-  },
-  devServer: {
-    port: 3000,
-    historyApiFallback: true // 404 responses will fall back to index.html. Required for using react-router-dom
-  },
-  devtool: 'inline-source-map', // Remove this when in production, takes alot of space!
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.glsl$/,
-        loader: 'webpack-glsl-loader'
-      },
-      {
-        test: /\.scss$/,
-        // Order of modules matters
-        use: [
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(jpg|png|svg|gif|pdf)$/,
-        use: ['file-loader']
-      },
-      {
-        //Load fonts
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-      filename: 'index.html'
-    }),
-    new ErrorOverlayPlugin()
-  ]
+	target: 'web',
+	entry: {
+		app: './src/index.js',
+	},
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: '[name].js',
+		publicPath: '/',
+	},
+	devServer: {
+		port: 3000,
+		historyApiFallback: true, // 404 responses will fall back to index.html. Required for using react-router-dom
+	},
+	devtool: 'inline-source-map', // Remove this when in production, takes alot of space!
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.glsl$/,
+				loader: 'webpack-glsl-loader',
+			},
+			{
+				test: /\.scss$/,
+				// Order of modules matters
+				use: [
+					process.env.NODE_ENV !== 'production'
+						? 'style-loader'
+						: MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(jpg|png|svg|gif|pdf)$/,
+				use: ['file-loader'],
+			},
+			{
+				//Load fonts
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true },
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.join(__dirname, 'src/index.html'),
+			filename: 'index.html',
+			favicon: 'src/favicon.ico',
+		}),
+		new ErrorOverlayPlugin(),
+	],
 };
--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -3,76 +3,79 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Removes unused files from dist/
 
 module.exports = {
-  target: 'web',
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, '../server/build'),
-    filename: '[name].[contenthash].frontend.js', // Content hash is a hash based on the file content
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.glsl$/,
-        loader: 'webpack-glsl-loader'
-      },
-      {
-        test: /\.scss$/,
-        // Order of modules matters
-        use: [
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(jpg|png|svg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        //Load fonts
-        test: /\.(woff|woff2|eot|ttf|otf|pdf)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    })
-  ],
-  optimization: {
-    moduleIds: 'hashed', // Make vendor hash stay consistent between builds
-    runtimeChunk: 'single', // Generate a single bundle for all runtime code. What's runtime? Code that is executed while your code is running, especially those instructions that you did not write explicitly, but are necessary for the proper execution of your code.
-    splitChunks: {
-      cacheGroups: {
-        // Split node modules such as lodash into a separate vendor, since these packages are rarely updated
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'node_modules',
-          chunks: 'all'
-        }
-      }
-    }
-  }
+	target: 'web',
+	entry: {
+		app: './src/index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, '../server/build'),
+		filename: '[name].[contenthash].frontend.js', // Content hash is a hash based on the file content
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.glsl$/,
+				loader: 'webpack-glsl-loader',
+			},
+			{
+				test: /\.scss$/,
+				// Order of modules matters
+				use: [
+					process.env.NODE_ENV !== 'production'
+						? 'style-loader'
+						: MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(jpg|png|svg|gif)$/,
+				use: ['file-loader'],
+			},
+			{
+				//Load fonts
+				test: /\.(woff|woff2|eot|ttf|otf|pdf)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true },
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+			favicon: 'src/favicon.ico',
+		}),
+	],
+	optimization: {
+		moduleIds: 'hashed', // Make vendor hash stay consistent between builds
+		runtimeChunk: 'single', // Generate a single bundle for all runtime code. What's runtime? Code that is executed while your code is running, especially those instructions that you did not write explicitly, but are necessary for the proper execution of your code.
+		splitChunks: {
+			cacheGroups: {
+				// Split node modules such as lodash into a separate vendor, since these packages are rarely updated
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'node_modules',
+					chunks: 'all',
+				},
+			},
+		},
+	},
 };
