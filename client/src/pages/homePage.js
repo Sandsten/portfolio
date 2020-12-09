befafcbfee8fa399315a038c5bf74efb12562741
Sandsten <staffansandberg@outlook.com>
2020-12-09 14:17:18 +0100

    Updated homepage text
    Also changed bandmate to currently down

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,117 +1,124 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { CSSTransition } from "react-transition-group";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { CSSTransition } from 'react-transition-group';
 
-import { DESKTOP_XS, DESKTOP_XL } from "../constants/sizes";
-import { BLUE } from "../constants/colors";
-import { Spacer } from "./projects";
+import { DESKTOP_XS, DESKTOP_XL } from '../constants/sizes';
+import { BLUE } from '../constants/colors';
+import { Spacer } from './projects';
 
-import "../CSSTransitions/transitions.scss";
+import '../CSSTransitions/transitions.scss';
 
 const StyledProjectPage = styled.div`
-  display: block;
-  min-height: 100vh;
-  padding: 0 20px 0 20px;
-  font-size: 1em;
+	display: block;
+	min-height: 100vh;
+	padding: 0 20px 0 20px;
+	font-size: 1em;
 
-  @media (min-width: ${DESKTOP_XS}) {
-    font-size: 1.2em;
-    width: 70vw;
-  }
+	@media (min-width: ${DESKTOP_XS}) {
+		font-size: 1.2em;
+		width: 70vw;
+	}
 
-  @media (min-width: ${DESKTOP_XL}) {
-    width: 50vw;
-  }
+	@media (min-width: ${DESKTOP_XL}) {
+		width: 50vw;
+	}
 `;
 
 export const StyledA = styled.a`
-  margin-bottom: 5px;
-  text-decoration: none;
-  outline: none;
-  color: ${BLUE};
+	margin-bottom: 5px;
+	text-decoration: none;
+	outline: none;
+	color: ${BLUE};
 
-  :hover {
-    text-decoration: underline;
-  }
+	:hover {
+		text-decoration: underline;
+	}
 `;
 
 const Contact = styled.div`
-  ${StyledA} {
-    margin-right: 20px;
-  }
+	${StyledA} {
+		margin-right: 20px;
+	}
 `;
 
 const CONTENT = [
-  <p>
-    Hello and welcome to my website! My name is Staffan Sandberg and I'm
-    currently in search for interesting job opportunities in anything tech
-    related. My main interest is in web development.{" "}
-  </p>,
-  <p>
-    My bachelor was in{" "}
-    <StyledA href="https://www.kth.se/student/kurser/program/TSVDK/20122/mal">
-      Simulation Technology and Virtual Design
-    </StyledA>{" "}
-    at{" "}
-    <StyledA href="https://www.kth.se/"> Kungliga Tekniska Högskolan</StyledA>.
-    Which sparked my interest for programming and graphics.
-  </p>,
-  <p>
-    After I received my degree I decided to take master level courses in
-    Interactive Media Technology. From evaluation methods to information
-    visualization. And I have now decided that I want to take what I've learned
-    and use it in the real world.
-  </p>,
-  <p>
-    In my free time I enjoy playing guitar, video games, reading books and
-    learning new things in general.
-  </p>,
-  <p>
-    On this site you can see different projects that I've been a part of and
-    blog posts about things I've learnt and want to share.
-  </p>,
-  <p>
-    {" "}
-    I'm always open to new opportunities.
-    <br />
-    You can find and contact me here:
-  </p>,
-  <Contact>
-    <StyledA href="https://www.linkedin.com/in/stsa/">Linkedin</StyledA>
-    <StyledA href="https://github.com/Sandsten">Github</StyledA>
-    <StyledA
-      target="_blank"
-      href="mailto:stsand@kth.se"
-      rel="noopener noreferrer"
-    >
-      Email
-    </StyledA>
-  </Contact>,
+	<p>Hello and welcome to my website! </p>,
+	<p>
+		My bachelor is in{' '}
+		<StyledA href="https://www.kth.se/student/kurser/program/TSVDK/20122/mal">
+			Simulation Technology and Virtual Design
+		</StyledA>{' '}
+		at{' '}
+		<StyledA href="https://www.kth.se/"> Kungliga Tekniska Högskolan</StyledA>.
+		Which sparked my interest for programming and graphics.
+	</p>,
+	<p>
+		My master's is in{' '}
+		<StyledA href="https://www.kth.se/en/studies/master/interactivemediatechnology/description-1.593765">
+			{' '}
+			Interactive Media Technology
+		</StyledA>{' '}
+		where I focused on visual media. Taking courses in computer graphics, game
+		design, information visualization and evaluation methods.
+	</p>,
+	<p>
+		For{' '}
+		<StyledA href="https://www.diva-portal.org/smash/record.jsf?dswid=9827&pid=diva2%3A1469206&c=1&searchType=UNDERGRADUATE&language=en&query=&af=%5B%5D&aq=%5B%5B%7B%22author%22%3A%5B%22Sandberg%2C+Staffan%22%5D%7D%5D%5D&aq2=%5B%5B%7B%22dateIssued%22%3A%7B%22from%22%3A%222020%22%2C%22to%22%3A%222020%22%7D%7D%5D%5D&aqe=%5B%5D&noOfRows=50&sortOrder=author_sort_asc&sortOrder2=title_sort_asc&onlyFullText=true&sf=all">
+			my thesis
+		</StyledA>{' '}
+		I wanted to explore Virtual Reality a bit further, since I had only briefly
+		used it previously in one of my courses. Therefore I found a topic where I
+		had to create a driving simulator for electic vehicles in VR. In order to
+		test a range critical situation using two different dashboards. You can read
+		about the project in a shorter format compared to the article{' '}
+		<StyledA href="/projects">here on my website</StyledA>{' '}
+	</p>,
+	<p>
+		In my free time I enjoy playing classical guitar, video games, reading books
+		and learning new things in general. One newfount interest is Disc golf which
+		I find very enjoyable and meditative.
+	</p>,
+	<p>
+		On this site you can see different projects that I've been a part of and
+		blog posts about things I've learnt and want to share.
+	</p>,
+	<p>You can find and contact me here:</p>,
+	<Contact>
+		<StyledA href="https://www.linkedin.com/in/stsa/">Linkedin</StyledA>
+		<StyledA href="https://github.com/Sandsten">Github</StyledA>
+		<StyledA
+			target="_blank"
+			href="mailto:stsand@kth.se"
+			rel="noopener noreferrer"
+		>
+			Email
+		</StyledA>
+	</Contact>,
 ];
 
 const homePage = () => {
-  const [shouldAnimate, setShouldAnimate] = useState(true);
-  const staggerDelay = 0.02;
+	const [shouldAnimate, setShouldAnimate] = useState(true);
+	const staggerDelay = 0.02;
 
-  return (
-    <StyledProjectPage>
-      {CONTENT.map((paragraph, i) => {
-        return (
-          <CSSTransition
-            key={i}
-            in={shouldAnimate}
-            appear={shouldAnimate}
-            classNames="fade"
-            timeout={500}
-            style={{ transitionDelay: `${(i + 1) * staggerDelay}s` }}
-          >
-            {paragraph}
-          </CSSTransition>
-        );
-      })}
-      <Spacer />
-    </StyledProjectPage>
-  );
+	return (
+		<StyledProjectPage>
+			{CONTENT.map((paragraph, i) => {
+				return (
+					<CSSTransition
+						key={i}
+						in={shouldAnimate}
+						appear={shouldAnimate}
+						classNames="fade"
+						timeout={500}
+						style={{ transitionDelay: `${(i + 1) * staggerDelay}s` }}
+					>
+						{paragraph}
+					</CSSTransition>
+				);
+			})}
+			<Spacer />
+		</StyledProjectPage>
+	);
 };
 
 export default homePage;
--- client/src/pages/projectPage.js
+++ client/src/pages/projectPage.js
@@ -1,128 +1,128 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Redirect } from "react-router";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { CSSTransition } from "react-transition-group";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { CSSTransition } from 'react-transition-group';
 
-import { fetchProjects } from "../redux/actions/projectsActions";
+import { fetchProjects } from '../redux/actions/projectsActions';
 
-import { DESKTOP_XS, DESKTOP_XL } from "../constants/sizes";
-import { BLUE } from "../constants/colors";
+import { DESKTOP_XS, DESKTOP_XL } from '../constants/sizes';
+import { BLUE } from '../constants/colors';
 
-import "../CSSTransitions/transitions.scss";
+import '../CSSTransitions/transitions.scss';
 
 const StyledProjectPage = styled.div`
-  display: block;
-  padding: 20px;
-  word-break: break-word;
-  min-height: 100vh;
+	display: block;
+	padding: 20px;
+	word-break: break-word;
+	min-height: 100vh;
 
-  @media (min-width: ${DESKTOP_XS}) {
-    width: 70vw;
-  }
+	@media (min-width: ${DESKTOP_XS}) {
+		width: 70vw;
+	}
 
-  @media (min-width: ${DESKTOP_XL}) {
-    width: 50vw;
-  }
+	@media (min-width: ${DESKTOP_XL}) {
+		width: 50vw;
+	}
 `;
 const ProjectTitle = styled.b`
-  font-size: 1.4em;
+	font-size: 1.4em;
 `;
 const StyledA = styled.a`
-  color: ${(p) => (p.theme === "LIGHT" ? "" : BLUE)};
+	color: ${(p) => (p.theme === 'LIGHT' ? '' : BLUE)};
 `;
 const StyledLink = styled(Link)`
-  color: ${(p) => (p.theme === "LIGHT" ? "" : BLUE)};
+	color: ${(p) => (p.theme === 'LIGHT' ? '' : BLUE)};
 `;
 
 const projectPage = (props) => {
-  // const [project, setProject] = useState(null);
-  const theme = useSelector((state) => state.appSettings.theme);
-  const project = useSelector((state) => {
-    if (!state.projects.data) return null;
-    // Find the correct project in the redux state
-    return state.projects.data.find((project) => {
-      return project.localURL === props.match.params.name;
-    });
-  });
-  const projectsFetched = useSelector((state) => state.projects.fetched);
-  const dispatch = useDispatch();
-  const staggerDelay = 0.02;
+	// const [project, setProject] = useState(null);
+	const theme = useSelector((state) => state.appSettings.theme);
+	const project = useSelector((state) => {
+		if (!state.projects.data) return null;
+		// Find the correct project in the redux state
+		return state.projects.data.find((project) => {
+			return project.localURL === props.match.params.name;
+		});
+	});
+	const projectsFetched = useSelector((state) => state.projects.fetched);
+	const dispatch = useDispatch();
+	const staggerDelay = 0.02;
 
-  useEffect(() => {
-    // If project wasn't in the redux state, fetch from database
-    if (!projectsFetched) dispatch(fetchProjects());
+	useEffect(() => {
+		// If project wasn't in the redux state, fetch from database
+		if (!projectsFetched) dispatch(fetchProjects());
 
-    //TODO: Opportunity for slight optimization, if refreshing on project page, only fetch that specific project
-    // if (!project) {
-    //   dispatch(fetchProject(props.match.params.name));
-    // }
-  }, []);
+		//TODO: Opportunity for slight optimization, if refreshing on project page, only fetch that specific project
+		// if (!project) {
+		//   dispatch(fetchProject(props.match.params.name));
+		// }
+	}, []);
 
-  if (project === undefined) return <Redirect to="/projects" />;
+	if (project === undefined) return <Redirect to="/projects" />;
 
-  // If projects haven't loaded yet
-  if (!project)
-    return (
-      <StyledProjectPage style={{ height: "100vh" }}>
-        'Loading...'
-      </StyledProjectPage>
-    );
-  if (!theme) return null;
+	// If projects haven't loaded yet
+	if (!project)
+		return (
+			<StyledProjectPage style={{ height: '100vh' }}>
+				'Loading...'
+			</StyledProjectPage>
+		);
+	if (!theme) return null;
 
-  var website = project.website ? (
-    <p>
-      Website: <StyledA href={project.website}>{project.website}</StyledA>
-    </p>
-  ) : null;
+	var website = project.website ? (
+		<p>
+			Website: <StyledA href={project.website}>{project.website}</StyledA>
+		</p>
+	) : null;
 
-  website =
-    project.website === "/shaders" ? (
-      <p>
-        Website: <StyledLink to="/shaders">Shader</StyledLink>
-      </p>
-    ) : (
-      website
-    );
+	if (project.website === '/shaders') {
+		website = (
+			<p>
+				Website: <StyledLink to="/shaders">Shader</StyledLink>
+			</p>
+		);
+	} else if (project.website === 'http://bandmate.xyz') {
+		website = <p>Website: currently down</p>;
+	}
+	const github = project.github ? (
+		<p>
+			Github: <StyledA href={project.github}>{project.github}</StyledA>
+		</p>
+	) : null;
 
-  const github = project.github ? (
-    <p>
-      Github: <StyledA href={project.github}>{project.github}</StyledA>
-    </p>
-  ) : null;
+	const CONTENT = [
+		<ProjectTitle>{project.title}</ProjectTitle>,
+		<p>{project.date}</p>,
+		<p>{project.tools.join(', ')}</p>,
+		<p>Project group size: {project.groupSize}</p>,
+		<p>{project.description}</p>,
+		website,
+		github,
+	];
 
-  const CONTENT = [
-    <ProjectTitle>{project.title}</ProjectTitle>,
-    <p>{project.date}</p>,
-    <p>{project.tools.join(", ")}</p>,
-    <p>Project group size: {project.groupSize}</p>,
-    <p>{project.description}</p>,
-    website,
-    github,
-  ];
-
-  return (
-    // Prevent StyledProjectPage from being a direct sibling to the sidebar
-    // otherwise its height will match it automatically which we don't want
-    <StyledProjectPage theme={theme}>
-      {CONTENT.map((text, i) => {
-        if (!text) return null; // Prevent github/website to try and render with cssTransition if they are null
-        return (
-          <CSSTransition
-            key={i}
-            in={true}
-            appear={true}
-            classNames="fade"
-            timeout={500}
-            style={{ transitionDelay: `${(i + 1) * staggerDelay}s` }}
-          >
-            {text}
-          </CSSTransition>
-        );
-      })}
-    </StyledProjectPage>
-  );
+	return (
+		// Prevent StyledProjectPage from being a direct sibling to the sidebar
+		// otherwise its height will match it automatically which we don't want
+		<StyledProjectPage theme={theme}>
+			{CONTENT.map((text, i) => {
+				if (!text) return null; // Prevent github/website to try and render with cssTransition if they are null
+				return (
+					<CSSTransition
+						key={i}
+						in={true}
+						appear={true}
+						classNames="fade"
+						timeout={500}
+						style={{ transitionDelay: `${(i + 1) * staggerDelay}s` }}
+					>
+						{text}
+					</CSSTransition>
+				);
+			})}
+		</StyledProjectPage>
+	);
 };
 
 export default projectPage;
