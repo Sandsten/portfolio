65a1e5ea84bc9dc16e654ece8f813200f54b243b
Sandsten <staffansandberg@outlook.com>
2021-03-05 12:32:36 +0100

    Added support for typescript

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -18,16 +18,27 @@ module.exports = {
 		historyApiFallback: true, // 404 responses will fall back to index.html. Required for using react-router-dom
 		// disableHostCheck: true // Might have to be enabled in order to run dev server in docker container
 	},
-	devtool: 'inline-source-map', // Remove this when in production, takes alot of space!
+	devtool: 'source-map', // Remove this when in production, takes alot of space!
+	resolve: {
+		// changed from extensions: [".js", ".jsx"]
+		// This is the order in which extensions are tested against imports
+		// e.g we can use "import File from '../path/to/file';" instead of "import File from '../path/to/file.ts';""
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+	},
 	module: {
 		rules: [
 			{
-				test: /\.(js|jsx)$/,
+				test: /\.(t|j)sx?$/,
+				use: { loader: 'ts-loader' },
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
 			},
+			// {
+			// 	test: /\.(js|jsx)$/,
+			// 	exclude: /node_modules/,
+			// 	use: {
+			// 		loader: 'babel-loader',
+			// 	},
+			// },
 			{
 				test: /\.glsl$/,
 				loader: 'webpack-glsl-loader',
@@ -63,6 +74,10 @@ module.exports = {
 			},
 		],
 	},
+	// externals: {
+	// 	react: 'React',
+	// 	'react-dom': 'ReactDOM',
+	// },
 	plugins: [
 		new HtmlWebPackPlugin({
 			template: path.join(__dirname, 'src/index.html'),
