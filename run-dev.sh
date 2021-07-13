45f3349a4c3cdad1be1a3d1a9de317daa1328a9f
Sandsten <staffansandberg@outlook.com>
2021-07-13 13:06:08 +0200

    bash script for starting dev mode

---
+++ run-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker-compose -f docker-compose.dev.yml up --build
+
+
+# "scripts": {
+#   "prod": "docker-compose -f docker-compose.prod.yml up --build -d",
+#   "dev": "docker-compose -f docker-compose.dev.yml up --remove-orphans",
+#   "dev:build": "docker-compose -f docker-compose.dev.yml up --build",
+#   "build": "docker build"
+# },
