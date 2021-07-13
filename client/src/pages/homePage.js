b5465c916f83fa168efe2a7c72386705a7bcb8cd
Sandsten <staffansandberg@outlook.com>
2021-07-13 17:38:41 +0200

    External links now work

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { CSSTransition } from 'react-transition-group';
 
-import { DESKTOP_XS, DESKTOP_XL } from '../constants/sizes';
+import { DESKTOP_XL } from '../constants/sizes';
 import { BASE03, BLUE, DARK_THEME } from '../constants/colors';
 
 import '../CSSTransitions/transitions.scss';
@@ -18,20 +19,30 @@ const StyledHomePage = styled.div`
 
 	color: ${(p) => (p.theme.main === 'LIGHT' ? BASE03 : DARK_THEME.TEXT)};
 
-	@media (min-width: ${DESKTOP_XS}) {
-		font-size: 1.1em;
-	}
-
 	@media (min-width: ${DESKTOP_XL}) {
 		grid-template-columns: 800px;
 	}
 `;
 
+// TODO: Apply the same style to both span and a without repeating!
+export const StyledLink = styled.span`
+	background: -webkit-linear-gradient(${DARK_THEME.LINK_1}, ${DARK_THEME.LINK_2});
+	background-clip: text;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+
+	:hover {
+		background: -webkit-linear-gradient(${DARK_THEME.LINK_2}, ${DARK_THEME.LINK_1});
+		cursor: pointer;
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+`;
+
 export const StyledA = styled.a`
-	margin-bottom: 5px;
 	text-decoration: none;
 	outline: none;
-	color: ${BLUE};
 
 	background: -webkit-linear-gradient(${DARK_THEME.LINK_1}, ${DARK_THEME.LINK_2});
 	background-clip: text;
@@ -39,89 +50,105 @@ export const StyledA = styled.a`
 	-webkit-text-fill-color: transparent;
 
 	:hover {
-		/* text-decoration: underline; */
 		background: -webkit-linear-gradient(${DARK_THEME.LINK_2}, ${DARK_THEME.LINK_1});
+		cursor: pointer;
 		background-clip: text;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
 `;
 
-const Contact = styled.div`
-	${StyledA} {
-		margin-right: 20px;
+const ContactContainer = styled.div`
+	font-size: 1.3em;
+`;
+
+const Paragraph = styled.p`
+	:first-child {
+		margin-top: 0px;
 	}
 `;
 
-const CONTENT = [
-	<div>Hello and welcome to my website! </div>,
-	<div>
-		My bachelor is in{' '}
-		<StyledA href="https://www.kth.se/student/kurser/program/TSVDK/20122/mal">
-			Simulation Technology and Virtual Design
-		</StyledA>{' '}
-		at <StyledA href="https://www.kth.se/"> Kungliga Tekniska Högskolan</StyledA>. Which sparked my
-		interest for programming and graphics.
-	</div>,
-	<div>
-		My master's is in{' '}
-		<StyledA href="https://www.kth.se/en/studies/master/interactivemediatechnology/description-1.593765">
-			{' '}
-			Interactive Media Technology
-		</StyledA>{' '}
-		where I focused on visual media. Taking courses in computer graphics, game design, information
-		visualization and evaluation methods.
-	</div>,
-	<div>
-		For{' '}
-		<StyledA href="https://www.diva-portal.org/smash/record.jsf?dswid=9827&pid=diva2%3A1469206&c=1&searchType=UNDERGRADUATE&language=en&query=&af=%5B%5D&aq=%5B%5B%7B%22author%22%3A%5B%22Sandberg%2C+Staffan%22%5D%7D%5D%5D&aq2=%5B%5B%7B%22dateIssued%22%3A%7B%22from%22%3A%222020%22%2C%22to%22%3A%222020%22%7D%7D%5D%5D&aqe=%5B%5D&noOfRows=50&sortOrder=author_sort_asc&sortOrder2=title_sort_asc&onlyFullText=true&sf=all">
-			my thesis
-		</StyledA>{' '}
-		I wanted to explore Virtual Reality a bit further, since I had only briefly used it previously
-		in one of my courses. Therefore I found a topic where I had to create a driving simulator for
-		electic vehicles in VR. In order to test a range critical situation using two different
-		dashboards. You can read about the project in a shorter format compared to the article{' '}
-		<StyledA href="/projects">here on my website</StyledA>{' '}
-	</div>,
-	<div>
-		In my free time I enjoy playing classical guitar, video games, reading books and learning new
-		things in general. One newfount interest is Disc golf which I find very enjoyable and
-		meditative.
-	</div>,
-	<div>
-		On this site you can see different projects that I've been a part of and blog posts about things
-		I've learnt and want to share.
-	</div>,
-	<div>You can find and contact me here:</div>,
-	<Contact>
-		<StyledA href="https://www.linkedin.com/in/stsa/">Linkedin</StyledA>
-		<StyledA href="https://github.com/Sandsten">Github</StyledA>
-		<StyledA target="_blank" href="mailto:stsand@kth.se" rel="noopener noreferrer">
-			Email
-		</StyledA>
-	</Contact>,
-];
+const Contact = () => {
+	return (
+		<ContactContainer>
+			<StyledA href="https://www.linkedin.com/in/stsa/">Linkedin </StyledA>
+			<StyledA href="https://github.com/Sandsten">Github </StyledA>
+			<StyledA target="_blank" href="mailto:stsand@kth.se" rel="noopener noreferrer">
+				Email
+			</StyledA>
+		</ContactContainer>
+	);
+};
+
+const LinkInternal = ({ text, handleNav, route }) => {
+	return (
+		<StyledLink
+			onClick={() => {
+				handleNav(route);
+			}}
+		>
+			{text}
+		</StyledLink>
+	);
+};
 
 const homePage = () => {
-	const [shouldAnimate, setShouldAnimate] = useState(true);
-	const staggerDelay = 0.02;
+	const [shouldAnimate] = useState(true);
+	const history = useHistory();
+	const handleNav = (route) => history.push(route);
 
 	return (
 		<StyledHomePage>
-			{CONTENT.map((paragraph, i) => {
-				return (
-					<CSSTransition
-						key={i}
-						in={shouldAnimate}
-						appear={shouldAnimate}
-						classNames="fade"
-						timeout={500}
-						style={{ transitionDelay: `${(i + 1) * staggerDelay}s` }}
-					>
-						{paragraph}
-					</CSSTransition>
-				);
-			})}
+			<CSSTransition in={shouldAnimate} appear={shouldAnimate} classNames="fade" timeout={500}>
+				<div>
+					<Paragraph>Hello and welcome to my website!</Paragraph>
+					<Paragraph>
+						My bachelor is in{' '}
+						<StyledA href="https://www.kth.se/student/kurser/program/TSVDK/20122/mal">
+							Simulation Technology and Virtual Design
+						</StyledA>{' '}
+						at <StyledA href="https://kth.se">Kungliga Tekniska Högskolan</StyledA>. Which sparked
+						my interest for programming and graphics.
+					</Paragraph>
+
+					<Paragraph>
+						My master's is in{' '}
+						<StyledA href="https://www.kth.se/en/studies/master/interactivemediatechnology/description-1.593765">
+							Interactive Media Technology
+						</StyledA>{' '}
+						where I focused on visual media. Taking courses in computer graphics, game design,
+						information visualization and evaluation methods.
+					</Paragraph>
+
+					<Paragraph>
+						For{' '}
+						<StyledA href="https://www.diva-portal.org/smash/record.jsf?dswid=9827&pid=diva2%3A1469206&c=1&searchType=UNDERGRADUATE&language=en&query=&af=%5B%5D&aq=%5B%5B%7B%22author%22%3A%5B%22Sandberg%2C+Staffan%22%5D%7D%5D%5D&aq2=%5B%5B%7B%22dateIssued%22%3A%7B%22from%22%3A%222020%22%2C%22to%22%3A%222020%22%7D%7D%5D%5D&aqe=%5B%5D&noOfRows=50&sortOrder=author_sort_asc&sortOrder2=title_sort_asc&onlyFullText=true&sf=all">
+							my thesis
+						</StyledA>{' '}
+						I wanted to explore Virtual Reality a bit further, since I had only briefly used it
+						previously in one of my courses. Therefore I found a topic where I had to create a
+						driving simulator for electric vehicles in VR. In order to test a range critical
+						situation using two different dashboards. You can read about the project in a shorter
+						format compared to the article{' '}
+						<LinkInternal
+							text="here on my website"
+							handleNav={(route) => handleNav(route)}
+							route="/projects"
+						/>
+					</Paragraph>
+					<Paragraph>
+						In my free time I enjoy playing classical guitar, video games, reading books and
+						learning new things in general. One newfound interest is Disc golf which I find very
+						enjoyable and meditative.
+					</Paragraph>
+					<Paragraph>
+						On this site you can see different projects that I've been a part of and blog posts
+						about things I've learnt and want to share.
+					</Paragraph>
+					<Paragraph>You can find and contact me here:</Paragraph>
+					<Contact />
+				</div>
+			</CSSTransition>
 		</StyledHomePage>
 	);
 };
