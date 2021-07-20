52a0d2ded677d3a78100640b8bd0374b80001b3d
Sandsten <staffansandberg@outlook.com>
2021-07-20 12:33:24 +0200

    dev bash script have options for removing orphans and rebuilding images

--- dev.sh
+++ dev.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
-docker-compose -f docker-compose.dev.yml up --build --remove-orphans
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Start dev environment with docker for portfolio"
+   echo
+   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "options:"
+   echo "-h     Print this Help."
+   echo "-o     Remove orphans."
+   echo "-b     Rebuild images (if new npm packages have been added you should include this)."
+   echo
+}
 
+# Base compose command
+DevCommand="docker-compose -f docker-compose.dev.yml up"
+
+# list of arguments expected in the input
+optstring=":hob"
+
+## getops will return a list with all options given to script (-h etc)
+while getopts ${optstring} option; do
+  case $option in
+    h) # display help. each option is called a "stanza" h) is a stanza
+      Help
+      exit
+      ;; # Each option must end with ;;
+    o)
+      DevCommand+=" --remove-orphans"
+      ;;      
+    b)
+      DevCommand+=" --build"
+      ;;
+    \?) # Match any option which hasn't been defined up above ^
+        echo "Error: Invalid option"
+        exit
+        ;;
+  esac
+done
+
+echo "Executing: "$DevCommand
+eval $DevCommand
 
 # "scripts": {
 #   "prod": "docker-compose -f docker-compose.prod.yml up --build -d",
