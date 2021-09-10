f64ae0051721f654300ddaa421f994c931e4e6fe
Sandsten <staffansandberg@outlook.com>
2021-09-10 11:30:31 +0200

    Some improvements to flex layout of images

--- frontend/src/Content/Projects/mastersThesis.tsx
+++ frontend/src/Content/Projects/mastersThesis.tsx
@@ -5,58 +5,78 @@ import { Link, useHistory } from 'react-router-dom';
 import { Container, Paragraph } from '../styledComponents';
 
 import { StyledLink } from "../../Components/Sidebar";
-import { MOBILE_XS } from "../../Constants/sizes";
+import { MOBILE_XS, DESKTOP_XS } from "../../Constants/sizes";
 import { StyledA } from "../RootPages/homePage";
 
+/*
+  FLEX DEFAULTS
+  * Items display in a row (the flex-direction property's default is row).
+  * The items start from the start edge of the main axis.
+  * The items do not stretch on the main dimension, but can shrink.
+  * The items will stretch to fill the size of the cross axis.
+  * The flex-basis property is set to auto.
+  * The flex-wrap property is set to nowrap
+*/
+
 const ImageRow = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: flex; // Takes up entire available width of page by default!
+  flex-direction: column;
   align-items: baseline;
+
+  @media(min-width: ${DESKTOP_XS}) {
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
 `;
 
-
+// Theses are the flex items inside an image row
 const StyledFigure = styled.figure`
-  /* max-height: 500px; */
-  /* width: auto; */
-  margin-left: 0;
+  /* flex-grow: 1; // Allow item to increase its size along the main axis */
+  /* flex-shrink: 1; // Allow item to decrease its size along the main axis */
+  /* flex-basis: auto; // */
+  /* flex: 1 1 auto; // Same as the three above */
+
+  /* flex: initial; // same as flex: 0 1 auto; */
+
+  // This will center the figure
+  margin-left: auto;
+  margin-right: auto;
   
   figcaption {
     font-size: 0.9em;
   }
 
-  :last-child {
-    margin-right: 0;
-  }
-
   :hover{
     cursor: pointer;
+  }
+
+  // This is when the sidebar moves to the left side
+  @media(min-width: ${DESKTOP_XS}) {
+    margin-left: 0;
+    margin-right: 20px;
+
+    :last-child {
+      margin-right: 0;
+    }
   }
 `;
 
 // Image will determine size of parent figure?
 const StyledImage = styled.img`
-  // Width will follow available space. i.e it will shrink when parent becomes to small
-  // It can't get too big either since it's limited by max-height
+  // width will match the width of the figure, the figure will adjust it width based on flexbox
+  // since aspect ratio is kept the width of the image is limited by the max-height
   width: 100%; 
-  
-  // Height to auto will adjust height to keep aspect ratio
-  height: auto;
-  
-  // This will prevent images from getting too large. 
-  max-height: 300px; 
-
+  max-height: 350px; 
 `;
 
 interface ImageProps {
-  height: string,
   imageName: string
 }
-const Image = ({imageName, height}:ImageProps) => {
+const Image = ({ imageName }:ImageProps) => {
 
   return (
     <StyledFigure>
       <StyledImage
-        height={height} 
         src={`https://staffansandberg.com/${imageName}`}>
       </StyledImage>
       <figcaption>
@@ -78,20 +98,20 @@ const mastersThesis = (props: Props) => {
       <p>Tjs asf</p>
       <h2>The setup</h2>
       <ImageRow>
-        <Image height="400px" imageName="simulator-setup.webp"></Image>
-        <Image height="400px" imageName="driving-in-vr.webp"></Image>
+        <Image imageName="simulator-setup.webp"></Image>
+        <Image imageName="driving-in-vr.webp"></Image>
       </ImageRow>
       <h2>Recruiting Participants</h2>
       <Paragraph>Poster placed around KTH Campus to recruit students, staff and anyone who happen to pass by. Since VR is a relatively novel technology I used it as an incentive to try and attract as many participants as possible. And to make signing up as easy as possible I used the free tier of <StyledA href="https://calendly.com/">Calendly</StyledA>  and a generated QR code which takes you directly to the sign up page</Paragraph>
       <ImageRow>
-        <Image height="500px" imageName="poster-version-1.webp"></Image>
-        <Image height="500px" imageName="poster-version-2.webp"></Image>
+        <Image imageName="poster-version-1.webp"></Image>
+        <Image imageName="poster-version-2.webp"></Image>
       </ImageRow>
       <p>
         Bla bla bla. Sign up page
       </p>
       <ImageRow>
-        <Image height="500px" imageName="signup-page.webp"></Image>
+        <Image imageName="signup-page.webp"></Image>
       </ImageRow>
     </Container>
   )
