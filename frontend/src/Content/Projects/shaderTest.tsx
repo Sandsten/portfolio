dba3880164cff2b48a466f662b65b32dcae4b75c
Staffan <staffansandberg@outlook.com>
2021-11-05 10:45:06 +0100

    Writing

--- frontend/src/Content/Projects/shaderTest.tsx
+++ frontend/src/Content/Projects/shaderTest.tsx
@@ -18,7 +18,19 @@ const shaderTest = (props: Props) => {
 			<p>
 				Experimenting with implementation of vertex and fragment shaders using Three.js. I follow an
 				amazing Youtube channel called{' '}
-				<Link href="https://www.youtube.com/c/TheArtofCodeIsCool/videos" text="The Art of Code" />
+				<Link href="https://www.youtube.com/c/TheArtofCodeIsCool/videos" text="The Art of Code" />{' '}
+				where he create visuals through shaders.
+			</p>
+			<p>
+				The example below is a ray marcher where the camera is positioned inside a torus. His
+				original implementation is done with shader toy{' '}
+				<Link
+					href="https://www.shadertoy.com/view/ts2XDw"
+					text="https://www.shadertoy.com/view/ts2XDw"
+				></Link>
+				. And here I've reimplemented it to work with Three.js and React in order to imbed it on my
+				website. I'm planning to explore ray marching further in the future with more shapes and
+				techniques.
 			</p>
 			<ShaderContainer>
 				<TreeShader />
