72c03966744778cdfc9fbc455599539f4b657804
Sandsten <staffansandberg@outlook.com>
2024-01-21 10:23:44 +0100

    renamed build script to not end with .sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# run ./build.sh <version> to build a docker image tagged with :<version> and :latest
-echo "Building sandsten/portfolio image with tag :"$1 "and :latest"
-docker build -t sandsten/portfolio:latest -t sandsten/portfolio:$1 .
-# Push the newly built image and its tags to docker hub
-docker push sandsten/portfolio:latest
-docker push sandsten/portfolio:$1
