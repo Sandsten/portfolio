3d19b48d357ff6941aabaaf42e9c6cae87e5d9f0
Sandsten <staffansandberg@outlook.com>
2024-01-21 10:23:06 +0100

    Set max width of project card space on large screens

--- frontend/src/Pages/RootPages/projectsPage.jsx
+++ frontend/src/Pages/RootPages/projectsPage.jsx
@@ -19,21 +19,18 @@ const StyledProjects = styled.div`
   // IMPORTAN.webpT: Don't set 100vh here! It's already done in the main container
   // If we set it here too there's a weird effect that occurs.
 	/* heigh.webpt: 100vh; */
-  
   margin-bottom: 10px;
 
-	// The following is applied when browser width goes above min-width
 	@media (min-width: ${DESKTOP_XS}) {
 		/* Autofit will create as many columns as will fit within the given max value, without each cell going bellow the min value */
 		/* In this case. Create as many columns no smaller than 450 pixels within 1fr of the given space */
 		grid-column-gap: 15px;
 		grid-row-gap: 15px;
 		grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
-		/* grid-template-row.webps: repeat(auto-fit, minmax(200px, 1fr)); */
 	}
   
   @media (min-width: ${DESKTOP_XL}) {
-    /* max-width: 1000px; */
+    max-width: 2000px;
   }
 `;
 
