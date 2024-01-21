20ead3be812dacf9b214c70910fabbd9a11460fc
Sandsten <staffansandberg@outlook.com>
2024-01-21 15:22:28 +0100

    To typescript

--- frontend/src/Constants/sizes.js
+++
@@ -1,3 +0,0 @@
-export const MOBILE_XS = '450px';
-export const DESKTOP_XS = '800px';
-export const DESKTOP_XL = '1300px';
---
+++ frontend/src/Constants/sizes.ts
@@ -0,0 +1,3 @@
+export const MOBILE_XS: string = '450px';
+export const DESKTOP_XS: string = '800px';
+export const DESKTOP_XL: string = '1300px';
